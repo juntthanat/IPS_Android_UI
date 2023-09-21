2988d37e117141c40b59d405b9b027f8f6d70566
Janthanat Thanintantrakun <73385307+juntthanat@users.noreply.github.com>
2023-09-21 23:15:14 +0700

    feat: change map floor
    - able to change map floor using button

--- lib/main.dart
+++ lib/main.dart
@@ -22,36 +22,24 @@ class MainPage extends StatefulWidget {
   const MainPage({super.key});
   @override
   // MainPageState createState() => MainPageState();
-  MainCompass createState() => MainCompass();
+  MainBody createState() => MainBody();
 }
 
-class MainPageState extends State<MainPage> {
-  @override
-  Widget build(BuildContext context) {
-    return Scaffold(
-      appBar: AppBar(
-        title: const Text("IPS Android UI"),
-      ),
-      drawer: Drawer(
-          child: ListView(
-        children: const [
-          DrawerHeader(
-              decoration: BoxDecoration(color: Colors.blue), child: Column()),
-        ],
-      )),
-      body:
-          // const Center(child: Image(image: AssetImage('assets/map/map_1.png'))),
-          Center(
-              child: Transform.rotate(
-        angle: pi / 2,
-        child: const Image(image: AssetImage('assets/map/map_1.png')),
-      )),
-    );
-  }
-}
-
-class MainCompass extends State<MainPage> {
+class MainBody extends State<MainPage> {
   double? heading = 0;
+
+  static List<Image> mapFloor = <Image>[
+    Image.asset(
+      "assets/map/map_1.png",
+      scale: 1.1,
+    ),
+    Image.asset(
+      "assets/map/map_2.png",
+      scale: 1.1,
+    ),
+  ];
+
+  int mapFloorIndex = 0;
 
   @override
   void initState() {
@@ -66,85 +54,112 @@ class MainCompass extends State<MainPage> {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      backgroundColor: Colors.black,
+      // backgroundColor: Colors.black,
       appBar: AppBar(
         backgroundColor: Colors.grey.shade900,
         centerTitle: true,
         title: const Text("Location Map"),
       ),
-      body: Column(
-        mainAxisAlignment: MainAxisAlignment.start,
-        crossAxisAlignment: CrossAxisAlignment.start,
-        children: [
-          Column(
-            mainAxisAlignment: MainAxisAlignment.start,
-            crossAxisAlignment: CrossAxisAlignment.start,
-            children: [
-              const SizedBox(
-                height: 5.0,
-                width: double.infinity,
+      body: Container(
+        color: Colors.black,
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.start,
+          crossAxisAlignment: CrossAxisAlignment.start,
+          children: [
+            Container(
+              color: Colors.grey[900],
+              child: Stack(
+                children: [
+                  Column(
+                    mainAxisAlignment: MainAxisAlignment.start,
+                    crossAxisAlignment: CrossAxisAlignment.start,
+                    children: [
+                      const SizedBox(
+                        height: 5.0,
+                        width: double.infinity,
+                      ),
+                      Column(
+                        mainAxisAlignment: MainAxisAlignment.center,
+                        crossAxisAlignment: CrossAxisAlignment.center,
+                        children: [
+                          const SizedBox(
+                            height: 5.0,
+                            width: 30,
+                          ),
+                          Padding(
+                            padding: const EdgeInsets.all(9.0),
+                            child: Stack(
+                              alignment: Alignment.center,
+                              children: [
+                                Image.asset(
+                                  "assets/compass/cadrant.png",
+                                  scale: 5.0,
+                                ),
+                                Transform.rotate(
+                                  angle: ((heading ?? 0) * (pi / 180) * -1),
+                                  child: Image.asset(
+                                      "assets/compass/compass.png",
+                                      scale: 5.0),
+                                ),
+                              ],
+                            ),
+                          ),
+                          Padding(
+                            padding: const EdgeInsets.all(9.0),
+                            child: Text(
+                              '${heading!.ceil()}',
+                              style: const TextStyle(
+                                  color: Colors.white,
+                                  fontSize: 13.0,
+                                  fontWeight: FontWeight.bold),
+                            ),
+                          ),
+                        ],
+                      ),
+                    ],
+                  ),
+                  Positioned.fill(
+                    child: Container(
+                      alignment: Alignment.center,
+                      height: 100,
+                      child: FloatingActionButton.extended(
+                        onPressed: () {
+                          setState(() {
+                            mapFloorIndex =
+                                (mapFloorIndex + 1) % mapFloor.length;
+                          });
+                        },
+                        label: const Text("Change Map"),
+                      ),
+                    ),
+                  ),
+                ],
               ),
-              Column(
+            ),
+            InteractiveViewer(
+              minScale: 0.1,
+              maxScale: 1.1,
+              boundaryMargin: const EdgeInsets.all(double.infinity),
+              child: Column(
                 mainAxisAlignment: MainAxisAlignment.center,
                 crossAxisAlignment: CrossAxisAlignment.center,
                 children: [
                   const SizedBox(
-                    height: 5.0,
-                    width: 30,
+                    height: 220,
                   ),
                   Padding(
-                    padding: const EdgeInsets.all(9.0),
-                    child: Stack(
-                      alignment: Alignment.center,
-                      children: [
-                        Image.asset(
-                          "assets/compass/cadrant.png",
-                          scale: 5.0,
-                        ),
-                        Transform.rotate(
-                          angle: ((heading ?? 0) * (pi / 180) * -1),
-                          child: Image.asset("assets/compass/compass.png",
-                              scale: 5.0),
-                        ),
-                      ],
+                    padding: const EdgeInsets.all(50.0),
+                    child: Transform.rotate(
+                      angle: ((heading ?? 0) * (pi / 180) * -1),
+                      // child: Image.asset("assets/map/map_1.png", scale: 1.1),
+                      child: mapFloor[mapFloorIndex],
                     ),
                   ),
-                  Padding(
-                    padding: const EdgeInsets.all(9.0),
-                    child: Text(
-                      '${heading!.ceil()}',
-                      style: const TextStyle(
-                          color: Colors.white,
-                          fontSize: 13.0,
-                          fontWeight: FontWeight.bold),
-                    ),
-                  )
                 ],
               ),
-            ],
-          ),
-          InteractiveViewer(
-            minScale: 0.1,
-            maxScale: 1.1,
-            boundaryMargin: const EdgeInsets.all(double.infinity),
-            child: Column(
-              mainAxisAlignment: MainAxisAlignment.center,
-              crossAxisAlignment: CrossAxisAlignment.center,
-              children: [
-                const SizedBox(
-                  height: 220,
-                ),
-                Padding(
-                  padding: const EdgeInsets.all(50.0),
-                  child: Transform.rotate(
-                    angle: ((heading ?? 0) * (pi / 180) * -1),
-                    child: Image.asset("assets/map/map_1.png", scale: 1.1),
-                  ),
-                ),
-              ],
-            ),
-          )
-        ],
+            )
+          ],
+        ),
       ),
     );
   }
